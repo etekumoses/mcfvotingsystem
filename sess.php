6e1df24f1766e25a102e467962f492e8405b1202
etekumoses <etekemoses@gmail.com>
2021-03-24 13:31:18 +0300

    refact:session

--- sess.php
+++ sess.php
@@ -2,15 +2,12 @@
 	require 'admin/dbcon.php';
 	session_start(); 
 	
-	//Check whether the session variable SESS_MEMBER_ID is present or not
-	if (!isset($_SESSION['voters_id']) || (trim($_SESSION['voters_id']) == '')) { ?>
-		<script>
-			window.location = "index.php";
-		</script>
-<?php 
-	}
+	if(!ISSET($_SESSION['voters_id'])){
+		header("location:index.php");
+	}else{
 		$session_id=$_SESSION['voters_id'];
 		$user_query = $conn->query("SELECT * FROM voters WHERE voters_id = '$session_id'") or die(mysqli_errno());
 		$user_row = $user_query->fetch_array();
 		$user_username = $user_row['firstname']." ".$user_row['lastname'];
+	}
 ?>
